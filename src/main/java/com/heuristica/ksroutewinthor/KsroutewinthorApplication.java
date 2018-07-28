6f5fa0f5b168eddb1c964ada477e3544db72ee27
Lázaro Nixon <lazaronixon@hotmail.com>
2018-07-28 05:14:33 -0300

    Move configurators to main class

--- src/main/java/com/heuristica/ksroutewinthor/KsroutewinthorApplication.java
+++ src/main/java/com/heuristica/ksroutewinthor/KsroutewinthorApplication.java
@@ -1,7 +1,21 @@
 package com.heuristica.ksroutewinthor;
 
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.PropertyNamingStrategy;
+import com.heuristica.ksroutewinthor.dozer.mapping.BranchMapping;
+import com.heuristica.ksroutewinthor.dozer.mapping.CustomerMapping;
+import com.heuristica.ksroutewinthor.dozer.mapping.LineMapping;
+import com.heuristica.ksroutewinthor.dozer.mapping.OrderMapping;
+import com.heuristica.ksroutewinthor.dozer.mapping.RegionMapping;
+import com.heuristica.ksroutewinthor.dozer.mapping.SubregionMapping;
+import java.util.Arrays;
+import org.apache.camel.CamelContext;
+import org.apache.camel.converter.dozer.DozerBeanMapperConfiguration;
+import org.apache.camel.converter.dozer.DozerTypeConverterLoader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class KsroutewinthorApplication {
@@ -9,5 +23,27 @@ public class KsroutewinthorApplication {
     public static void main(String[] args) {
         SpringApplication.run(KsroutewinthorApplication.class, args);
     }
- 
+
+    @Bean
+    public ObjectMapper defaultJacksonFormat() {
+        return new ObjectMapper()
+                .setPropertyNamingStrategy(PropertyNamingStrategy.SNAKE_CASE)
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
+
+    @Bean
+    public DozerBeanMapperConfiguration mapper() {
+        DozerBeanMapperConfiguration dozerConfig = new DozerBeanMapperConfiguration();
+        dozerConfig.setBeanMappingBuilders(Arrays.asList(
+                new BranchMapping(), new CustomerMapping(), new LineMapping(),
+                new OrderMapping(), new RegionMapping(), new SubregionMapping()
+        ));
+        return dozerConfig;
+    }
+
+    @Bean
+    public DozerTypeConverterLoader dozerConverterLoader(CamelContext camelContext, DozerBeanMapperConfiguration dozerConfig) {
+        return new DozerTypeConverterLoader(camelContext, dozerConfig);
+    }
+
 }
