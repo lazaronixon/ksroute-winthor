9d34edb1ea7555f0564835fc45fb1d0c7ca48b87
Lázaro Nixon <lazaronixon@hotmail.com>
2018-07-28 05:13:11 -0300

    More work

--- src/main/java/com/heuristica/ksroutewinthor/JacksonConfigurator.java
+++ src/main/java/com/heuristica/ksroutewinthor/JacksonConfigurator.java
@@ -3,20 +3,11 @@ package com.heuristica.ksroutewinthor;
 import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
-import org.apache.camel.model.dataformat.JsonDataFormat;
-import org.apache.camel.model.dataformat.JsonLibrary;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class JacksonConfigurator {
-    
-    @Bean
-    public JsonDataFormat json() {
-        JsonDataFormat dataFormat = new JsonDataFormat(JsonLibrary.Jackson);
-        dataFormat.setObjectMapper("defaultJacksonFormat");
-        return dataFormat;
-    }
 
     @Bean
     public ObjectMapper defaultJacksonFormat() {
