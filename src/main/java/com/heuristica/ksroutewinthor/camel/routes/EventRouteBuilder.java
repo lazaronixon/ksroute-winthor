6d6d5e477d39eaf3a6ed192086e5e5d638782440
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-10-22 08:51:50 -0300

    cosmetic

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/EventRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/EventRouteBuilder.java
@@ -12,7 +12,7 @@ public class EventRouteBuilder extends RouteBuilder  {
     public void configure() throws Exception {
         from("jpa:" + Event.class.getName() + "?delay=15s&maximumResults=50&namedQuery=newEvents").routeId("process-events").to("seda:process-events-seda");
         
-        from("seda:process-events-seda?concurrentConsumers=5").routeId("process-events-seda")
+        from("seda:process-events-seda?concurrentConsumers=3").routeId("process-events-seda")
             .log("Processando ${body}")
             .toD("direct:Event-${body.persistAction}-${body.eventableType}");
     }
