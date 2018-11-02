163f3db8b8ff4f489103b47c9dec5a865061330e
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-11-02 16:58:01 -0300

    more work

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/KsrouteApiRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/KsrouteApiRouteBuilder.java
@@ -1,5 +1,7 @@
 package com.heuristica.ksroutewinthor.camel.routes;
 
+import org.apache.camel.LoggingLevel;
+import org.apache.camel.http.common.HttpOperationFailedException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -11,6 +13,11 @@ class KsrouteApiRouteBuilder extends ApplicationRouteBuilder {
     @Override
     public void configure() throws Exception {      
         super.configure();
+        
+        onException(HttpOperationFailedException.class).handled(true)
+                .log(LoggingLevel.WARN, "Erro no servidor: ${exception.message}, detalhe: ${body}")
+                .choice().when(simple("${exception.statusCode} == 404")).stop()
+                .otherwise().throwException(new RuntimeException());
         
         from("direct:ksroute-api").routeId("ksroute-api")
                 .setHeader("X-User-Token", constant("{{ksroute.api.token}}"))
