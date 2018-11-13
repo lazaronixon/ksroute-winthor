a77a43fd73b0d5e838be86c9468c766ae2055e56
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-11-13 18:03:14 -0200

    enrich exception

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/ApplicationRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/ApplicationRouteBuilder.java
@@ -6,7 +6,7 @@ public abstract class ApplicationRouteBuilder extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
-        onException(Exception.class).to("bean:exceptionReporter");        
+        onException(Exception.class).to("bean:exceptionReporter");
     }
-    
+
 }
--- src/main/java/com/heuristica/ksroutewinthor/utils/ExceptionReporter.java
+++ src/main/java/com/heuristica/ksroutewinthor/utils/ExceptionReporter.java
@@ -1,19 +1,27 @@
 package com.heuristica.ksroutewinthor.utils;
 
 import io.sentry.Sentry;
+import org.apache.camel.Exchange;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 @Component
-public class ExceptionReporter {  
+public class ExceptionReporter { 
+    
+    private static final String JMS_MESSAGE_ID = "JMSMessageID";
     
     @EventListener(ApplicationReadyEvent.class)
     public void initializeSentry() {
         Sentry.init();
     }    
 
-    public void report(Exception e) {        
-        Sentry.capture(e);
+    public void report(Exchange exchange) {
+        Exception cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
+        Sentry.getContext().addExtra("messageId", exchange.getIn().getHeader(JMS_MESSAGE_ID, String.class));
+        Sentry.getContext().addExtra("endpoint", exchange.getProperty(Exchange.FAILURE_ENDPOINT, String.class));
+        Sentry.getContext().addExtra("routeId", exchange.getProperty(Exchange.FAILURE_ROUTE_ID, String.class));
+        Sentry.getContext().addExtra("body", exchange.getIn().getBody(String.class));
+        Sentry.capture(cause);
     }
 }
