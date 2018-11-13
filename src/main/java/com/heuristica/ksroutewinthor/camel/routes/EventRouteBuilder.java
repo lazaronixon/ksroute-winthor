c1c70cb909c96a4ceeb823211ad3244afc951bf3
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-11-13 08:49:37 -0200

    REVERT

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/EventRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/EventRouteBuilder.java
@@ -9,14 +9,14 @@ public class EventRouteBuilder extends ApplicationRouteBuilder  {
 
     @Override
     public void configure() throws Exception { 
-        super.configure();
+        super.configure();        
         
         from("jpa:" + Event.class.getName() + "?delay=15s&namedQuery=newEvents").routeId("process-events")                
                 .marshal().json(JsonLibrary.Jackson).to("activemq:default");
 
         from("activemq:default?concurrentConsumers=5&acknowledgementMode=2").routeId("process-event")
                 .transacted("PROPAGATION_REQUIRED")
-                .unmarshal().json(JsonLibrary.Jackson, Event.class)
+                .unmarshal().json(JsonLibrary.Jackson, Event.class)                
                 .log("Processando ${body}").toD("direct:${body.route}");
     }
     
