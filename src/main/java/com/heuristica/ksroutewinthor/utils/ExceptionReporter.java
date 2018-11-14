b86ede2751b6b7da00ee5bedb3404b296f3ea09e
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-11-14 16:56:05 -0200

    property file

--- src/main/java/com/heuristica/ksroutewinthor/utils/ExceptionReporter.java
+++ src/main/java/com/heuristica/ksroutewinthor/utils/ExceptionReporter.java
@@ -2,6 +2,7 @@ package com.heuristica.ksroutewinthor.utils;
 
 import io.sentry.Sentry;
 import org.apache.camel.Exchange;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
@@ -9,12 +10,14 @@ import org.springframework.stereotype.Component;
 @Component
 public class ExceptionReporter { 
     
+    @Value("${ksroute.sentry.dns}")
+    private String SENTRY_DNS;    
     private static final String JMS_MESSAGE_ID = "JMSMessageID";
     
     @EventListener(ApplicationReadyEvent.class)
     public void initializeSentry() {
-        Sentry.init();
-    }    
+        Sentry.init(SENTRY_DNS);
+    }
 
     public void report(Exchange exchange) {
         Exception cause = exchange.getProperty(Exchange.EXCEPTION_CAUGHT, Exception.class);
