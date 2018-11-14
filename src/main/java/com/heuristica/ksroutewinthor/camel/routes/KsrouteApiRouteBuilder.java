149f6997d60c19326d36c01c8d47504669ec119e
NIXON <NIXON@DESKTOP-VRG1LRM>
2018-11-14 11:44:52 -0200

    not retry 404

--- src/main/java/com/heuristica/ksroutewinthor/camel/routes/KsrouteApiRouteBuilder.java
+++ src/main/java/com/heuristica/ksroutewinthor/camel/routes/KsrouteApiRouteBuilder.java
@@ -1,5 +1,6 @@
 package com.heuristica.ksroutewinthor.camel.routes;
 
+import org.apache.camel.http.common.HttpOperationFailedException;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -13,8 +14,13 @@ class KsrouteApiRouteBuilder extends ApplicationRouteBuilder {
         super.configure();            
         
         from("direct:ksroute-api").routeId("ksroute-api")
-                .setHeader("X-User-Token", constant("{{ksroute.api.token}}"))
-                .throttle(MAXIMUM_REQUEST_COUNT).timePeriodMillis(TIME_PERIOD_MILLIS)
-                .to("https4:ksroute-api-request");
+                .doTry()
+                    .setHeader("X-User-Token", constant("{{ksroute.api.token}}"))
+                    .throttle(MAXIMUM_REQUEST_COUNT).timePeriodMillis(TIME_PERIOD_MILLIS)
+                    .to("https4:ksroute-api-request")
+                .endDoTry().doCatch(HttpOperationFailedException.class)
+                    .choice().when(simple("${exception.statusCode} == 404")).to("bean:exceptionReporter")
+                    .otherwise().throwException(RuntimeException.class, "HTTP Server Error").endChoice()
+                .end();
     }      
 }
